d83686cc1ef842d05fcee29a358a2a492fa566c0
Shivangi008 <shivangi.sinha008@gmail.com>
2022-08-08 17:41:45 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -4,56 +4,137 @@ import './App.css';
 function App() {
   const question=[
     {
-      questionText:'What is the capital of france?',
+      questionText:'Which of the following command is used to create react-js-app ?',
 
       answerOptions:[
     
 
     
-     { answerText:'New York',isCorrect:false},
-     { answerText:'London',isCorrect:false},
-     { answerText:'Paris',isCorrect:true},
-     { answerText:'Dublin',isCorrect:false},
+     { answerText:'npx install create-react-app -g',isCorrect:false},
+     { answerText:'install - l create-react-app',isCorrect:false},
+     { answerText:'	npx create-react-app appname',isCorrect:true},
+     { answerText:'npm install create-react-app',isCorrect:false},
       ],
     },
 
 
     {
-      questionText:'Who is CEO of Tesla?',
+      questionText:'Which of the following is must for the API in React.js ?',
 
       answerOptions:[
     
-     { answerText:'jeff Benos',isCorrect:false},
-     { answerText:'Elon Musk',isCorrect:true},
-     { answerText:'Bill gates',isCorrect:false},
-     { answerText:'Tony stark',isCorrect:false},
+     { answerText:'render',isCorrect:false},
+     { answerText:'renderComponent',isCorrect:true},
+     { answerText:'SetinitialComponent',isCorrect:false},
+     { answerText:'All of the above',isCorrect:false},
       ],
     },
    
 {
-    questionText:'The iPhone was created by which company?',
+    questionText:'What of the following is used in React.js to increase performance?',
 
     answerOptions:[
   
-   { answerText:'Apple',isCorrect:true},
-   { answerText:'Intel',isCorrect:false},
-   { answerText:'Amazon',isCorrect:false},
-   { answerText:'Microsoft',isCorrect:false},
+   { answerText:'Virtual DOM',isCorrect:true},
+   { answerText:'Original DOM',isCorrect:false},
+   { answerText:'Both A and B',isCorrect:false},
+   { answerText:'None of the above',isCorrect:false},
     ],
   },
 
 
   {
-    questionText:'How many Harry Potter books are there?',
+    questionText:' Which of the following acts as the input of a class-based component?',
 
     answerOptions:[
   
-   { answerText:'1',isCorrect:true},
-   { answerText:'4',isCorrect:false},
-   { answerText:'6',isCorrect:false},
-   { answerText:'7',isCorrect:true},
+   { answerText:'Class',isCorrect:false},
+   { answerText:'Factory',isCorrect:false},
+   { answerText:'Render',isCorrect:false},
+   { answerText:'Props',isCorrect:true},
     ],
   },
+
+  {
+    questionText:'  What is the declarative way to render a dynamic list of components based on values in an array?',
+
+    answerOptions:[
+  
+   { answerText:'With a for/while loop',isCorrect:false},
+   { answerText:'Using the <Each /> component',isCorrect:false},
+   { answerText:'Using the reduce array method',isCorrect:false},
+   { answerText:'Using the Array.map() method',isCorrect:true},
+    ],
+  },
+
+
+
+
+  {
+    questionText:'What is a state in React?',
+
+    answerOptions:[
+  
+   { answerText:'Internal storage of the component.',isCorrect:true},
+   { answerText:'A permanent storage.',isCorrect:false},
+   { answerText:'External storage of the component.',isCorrect:false},
+   { answerText:'None of the above.',isCorrect:false},
+    ],
+  },
+
+
+
+  {
+    questionText:'Which of the following function is called to render HTML to the web page in React?',
+
+    answerOptions:[
+  
+   { answerText:'Render()',isCorrect:false},
+   { answerText:'render()',isCorrect:true},
+   { answerText:'ReactDOM_render()',isCorrect:false},
+   { answerText:'Reactdom_render()',isCorrect:false},
+    ],
+  },
+
+
+
+  {
+    questionText:'Everything in React is a _____________',
+
+    answerOptions:[
+  
+   { answerText:'Class',isCorrect:false},
+   { answerText:'Package',isCorrect:false},
+   { answerText:'Module',isCorrect:false},
+   { answerText:'COmponent',isCorrect:true},
+    ],
+  },
+
+
+  {
+    questionText:'What is Babel?',
+
+    answerOptions:[
+  
+   { answerText:'A transpiler',isCorrect:false},
+   { answerText:'Both Compiler and Transpiler',isCorrect:true},
+   { answerText:'An interpreter',isCorrect:false},
+   { answerText:'A compiler',isCorrect:false},
+    ],
+  },
+
+  {
+    questionText:'Props are __________ into other components',
+
+    answerOptions:[
+  
+   { answerText:'Injected',isCorrect:false},
+   { answerText:'Methods',isCorrect:true},
+   { answerText:'Both A and B',isCorrect:false},
+   { answerText:'All OF these',isCorrect:false},
+    ],
+  },
+
 
 
   ];
@@ -79,7 +160,7 @@ function App() {
 		<div className='app'>
 			{showScore ? (
 				<div className='score-section'>
-					You scored {score} out of {question.length}
+				Congrats You scored {score} out of {question.length} !!
 				</div>
 			) : (
 				<>
